a38217fccfd6da259ed84b270d52c0ec483f30d0
FlutLab.io <>
2021-12-05 16:56:32 +0000

    Autosave

--- lib/ui/HomePage.dart
+++ lib/ui/HomePage.dart
@@ -276,7 +276,7 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
                     return [
                       SliverAppBar(
                         pinned: true,
-                        backgroundColor: Get.isDarkMode ? Colors.black36 : Colors.white,
+                        backgroundColor: Get.isDarkMode ? Colors.black38 : Colors.white,
                         automaticallyImplyLeading: false,
                         bottom: PreferredSize(
                           preferredSize: Size.fromHeight(44.0),
